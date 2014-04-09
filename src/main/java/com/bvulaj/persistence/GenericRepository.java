9e6ef13db03d3e1124d652ab53fec6b801101450
Brandon Vulaj <brandon.vulaj@gmail.com>
2014-04-08 22:09:36 -0400

    Initial push of interface.

---
+++ src/main/java/com/bvulaj/persistence/GenericRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.bvulaj.persistence;
+
+import java.io.Serializable;
+
+/**
+ * @author Brandon Vulaj
+ * 
+ */
+public interface GenericRepository<T, K extends Serializable> {
+
+}
